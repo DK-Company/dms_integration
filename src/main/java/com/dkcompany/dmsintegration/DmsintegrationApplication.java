61f9468a3fc3b607e661197921e288239b8e47fb
Emil Bang Høyer <ebh@it-minds.dk>
2023-09-04 10:04:53 +0200

    Initial commit

---
+++ src/main/java/com/dkcompany/dmsintegration/DmsintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.dkcompany.dmsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmsintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmsintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dkcompany/dmsintegration/DmsintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dkcompany.dmsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmsintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
